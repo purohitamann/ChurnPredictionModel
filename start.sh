d09705c63ee0a940f9c7744703e63b82971e118c
Aman Purohit <purohiam@sheridancollege.ca>
2024-10-23 23:29:47 -0400

    Refactor start.sh: Remove commented out gunicorn command

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@ pip install fastapi
 pip install gunicorn
 
 # Start Streamlit on a different port (8501)
-streamlit run main.py --server.port 8501 &
+streamlit run main.py --server.port 8501 
 
 # Start FastAPI using Uvicorn on port 8000
-gunicorn -w 4 -k uvicorn.workers.UvicornWorker app:app --bind 0.0.0.0:8000
+# gunicorn -w 4 -k uvicorn.workers.UvicornWorker app:app --bind 0.0.0.0:8000
